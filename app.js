7528c4f5c41124544809c1408f1bdc7a5123f526
daviaser <daviahowardser@gmail.com>
2023-11-12 21:40:33 -0500

    exercise 2 complete

---
+++ app.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('searchButton').addEventListener('click', function() {
+    // Make an AJAX request using the Fetch API
+    fetch('superheroes.php')
+      .then(response => response.text()) // Change to text() since we are fetching HTML
+      .then(data => {
+        // Display the HTML content in a JavaScript alert
+        alert('Superheroes:\n' + data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  });
+});
+
